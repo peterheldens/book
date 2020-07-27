22ba0775148be307e9fc513eb893b249bbd59250
Peter Heldens <peterhel@microsoft.com>
2020-07-27 09:07:10 +0200

    added name badge

---
+++ level0/01-name-badge/main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Microbitfun")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(8)
+})
+basic.showIcon(IconNames.Heart)
+basic.forever(function () {
+	
+})
