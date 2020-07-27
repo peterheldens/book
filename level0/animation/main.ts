2ac52b60aeea705c952840715d99b9f1da5c5df0
Peter Heldens <peterhel@microsoft.com>
2020-07-27 09:15:32 +0200

    added animation

---
+++ level0/animation/main.ts
@@ -0,0 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . # . #
+            . # # # .
+            # . # . .
+            . # # # .
+            # . # . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            # . # . .
+            . # # # .
+            . . # . #
+            . # # # .
+            # . # . #
+            `)
+        basic.pause(100)
+    }
+})
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    # . . . #
+    . # # # .
+    `)
